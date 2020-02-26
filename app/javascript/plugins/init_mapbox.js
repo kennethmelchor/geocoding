caad35f6a967e024299144748440ed7c99c0b7f9
kennethmelchor <kennethmelchor@gmail.com>
2020-02-26 10:31:42 +0000

    added markers

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -9,6 +9,18 @@ const initMapbox = () => {
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v10'
     });
+
+// [{"lat":48.8649224,"lng":2.3800903},{"lat":51.5410678,"lng":-0.076459}]
+    const markers = JSON.parse(mapElement.dataset.markers);
+    markers.forEach((marker) => {
+    // {"lat":48.8649224,"lng":2.3800903}
+      new mapboxgl.Marker()
+        .setLngLat([ marker.lng, marker.lat ])
+        .addTo(map);
+    });
+
+
+
   }
 };
 
