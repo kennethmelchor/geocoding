4ae0e1a4bc2bc412ea0358b94352c134b5870d2c
kennethmelchor <kennethmelchor@gmail.com>
2020-02-26 11:11:53 +0000

    final

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,4 +1,11 @@
 import mapboxgl from 'mapbox-gl';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+
+const fitMapToMarkers = (map, markers) => {
+  const bounds = new mapboxgl.LngLatBounds();
+  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 1500 });
+};
 
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
@@ -7,19 +14,24 @@ const initMapbox = () => {
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v10'
+      style: 'mapbox://styles/kennethmelchor/ck737fmrs17wf1jsh6q6a8hmu'
     });
 
+  map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
+      mapboxgl: mapboxgl }));
 // [{"lat":48.8649224,"lng":2.3800903},{"lat":51.5410678,"lng":-0.076459}]
     const markers = JSON.parse(mapElement.dataset.markers);
     markers.forEach((marker) => {
     // {"lat":48.8649224,"lng":2.3800903}
+      const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); // add this
+
       new mapboxgl.Marker()
         .setLngLat([ marker.lng, marker.lat ])
+        .setPopup(popup) // add this
         .addTo(map);
     });
 
-
+    fitMapToMarkers(map, markers);
 
   }
 };
